9c6da2c34ebceb6870e8d0bba00b2da5e4f83454
PiotrB <piotr.baczkiewicz@regitech.pl>
2020-07-10 13:24:35 +0200

    Add Project

---
+++ FolkProject/FolkProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FolkProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
